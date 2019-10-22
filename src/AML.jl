1564141acdaa2b1b821ccaa9d17947c53157d94f
aminya <aminyahyaabadi74@gmail.com>
2019-10-22 10:30:05 -0500

    findfirstcontent strings

--- src/AML.jl
+++ src/AML.jl
@@ -4,5 +4,28 @@ using EzXML
 import EzXML.Node
 
 export @aml, print
+################################################################
+# Extractors
+"""
+    findfirstcontent(element,node)
+    findfirstcontent(type,element,node)
+
+
+Returns first element content. It also convert to the desired format by passing type. element is given as string.
+```julia
+findfirstcontent("/instrument-name",node)
+findfirstcontent(UInt8,"/midi-channel",node)
+```
+"""
+# for strings
+function findfirstcontent(::Type{T}, s::String,node::Node) where{T<:Union{String, Nothing}}
+    elm = findfirst(s,node)
+    if isnothing(elm)
+        return nothing
+    else
+        return elm.content
+    end
+end
+findfirstcontent(s::String,node::Node) = findfirstcontent(Union{String, Nothing}, s::String, node::Node)
 
 end
