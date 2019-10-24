1256bb18dd9e4cdd0a0fcccc90abb53850d7bf9f
aminya <aminyahyaabadi74@gmail.com>
2019-10-24 18:31:42 -0500

    Moving utility functions to utilities.jl

---
+++ src/utilities.jl
@@ -0,0 +1,161 @@
+using EzXML
+import EzXML.Node
+
+export findfirstcontent, findallcontent, addelementOne!, addelementVect!, print, show
+
+
+################################################################
+# Extractors
+"""
+    findfirstcontent(element,node)
+    findfirstcontent(type,element,node)
+
+
+Returns first element content. It also convert to the desired format by passing type. element is given as string.
+```julia
+findfirstcontent("/instrument-name",node)
+findfirstcontent(UInt8,"/midi-channel",node)
+```
+"""
+# for strings
+function findfirstcontent(::Type{T}, s::String,node::Node) where{T<:Union{String, Nothing}}
+    elm = findfirst(s,node)
+    if isnothing(elm)
+        return nothing
+    else
+        return elm.content
+    end
+end
+findfirstcontent(s::String,node::Node) = findfirstcontent(Union{String, Nothing}, s::String, node::Node)
+
+# for numbers
+function findfirstcontent(::Type{T},s::String,node::Node) where {T<:Union{Number,Nothing}}
+    elm = findfirst(s,node)
+    if isnothing(elm)
+        return nothing
+    else
+        return parse(T, elm.content)
+    end
+end
+
+# for defined types
+function findfirstcontent(::Type{T},s::String,node::Node) where {T}
+    elm = findfirst(s,node)
+    if isnothing(elm)
+        return nothing
+    else
+        return T(elm)
+    end
+end
+
+################################################################
+"""
+    findallcontent(type, string, node)
+
+Finds all the elements with the address of string in the node, and converts the elements to Type object.
+"""
+# for strings
+function findallcontent(::Type{T}, s::String, node::Node) where{T<:Union{String, Nothing}}
+
+    elmsNode = findall(s, node) # a vector of Node elements
+    if isnothing(elmsNode)
+        return nothing
+    else
+        elmsType = Vector{T}(undef, length(elmsNode)) # a vector of Type elements
+        i=1
+        for elm in elmsNode
+            elmsType[i]=elm.content
+            i=+1
+        end
+        return elmsType
+    end
+
+end
+findallcontent(s::String, node::Node) = findallcontent(Union{String, Nothing},s, node)
+
+# for numbers
+function findallcontent(::Type{T}, s::String, node::Node) where{T<:Union{Number,Nothing}}
+
+    elmsNode = findall(s, node) # a vector of Node elements
+    if isnothing(elmsNode)
+        return nothing
+    else
+        elmsType = Vector{T}(undef, length(elmsNode)) # a vector of Type elements
+        i=1
+        for elm in elmsNode
+            elmsType[i]=parse(T, elm.content)
+            i=+1
+        end
+        return elmsType
+    end
+
+end
+
+# for defined types
+function findallcontent(::Type{T}, s::String, node::Node) where{T}
+
+    elmsNode = findall(s, node) # a vector of Node elements
+    if isnothing(elmsNode)
+        return nothing
+    else
+        elmsType = Vector{T}(undef, length(elmsNode)) # a vector of Type elements
+        i=1
+        for elm in elmsNode
+            elmsType[i]=T(elm)
+            i=+1
+        end
+        return elmsType
+    end
+
+end
+################################################################
+# Constructors
+
+# strings
+function addelementOne!(aml::Node, name::String, value::String)
+    if !isnothing(value)
+        addelement!(aml, name, value)
+    end
+end
+
+# number
+function addelementOne!(aml::Node, name::String, value::T) where {T<:Number}
+    addelement!(aml, name, string(value))
+end
+
+#  defined or nothing
+function addelementOne!(aml::Node, name::String, value)
+    if !isnothing(value)
+        link!(aml,value.aml)
+    end
+end
+
+# vector of strings
+function addelementVect!(aml::Node, name::String, value::Vector{String})
+    for ii = 1:length(value)
+        addelement!(aml, name, value[ii])
+    end
+end
+
+# vector of numbers
+function addelementVect!(aml::Node, name::String, value::Vector{T}) where {T<:Number}
+    for ii = 1:length(value)
+        addelement!(aml, name, string(value[ii]))
+    end
+end
+
+#  vector of defined or nothing
+function addelementVect!(aml::Node, name::String, value::Vector{T}) where {T}
+    for ii = 1:length(value)
+        link!(aml,value[ii].aml)
+    end
+end
+################################################################
+function Base.print(x::Node)
+    println("")
+    prettyprint(x)
+end
+function Base.show(io::IO,x::Node)
+    println("")
+    prettyprint(io, x)
+end
