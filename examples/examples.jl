1de07218154ecb516b09bb937567e59c7c3d357f
aminya <aminyahyaabadi74@gmail.com>
2019-10-25 03:56:43 -0500

    Doc/Example update

--- examples/examples.jl
+++ examples/examples.jl
@@ -5,34 +5,34 @@ using AML
     field::String, "study-field"
     GPA::Float64 = 4.5, "GPA"
     courses::Vector{String}, "taken-courses"
+    ID::Int64, a"id"
 end
 
 
 @aml struct University "university"
-    name, "university-name"
+    name, a"university-name"
     people::Vector{Person}, "students"
 end
 
-P1 = Person(age=24, field="Mechanical Engineering", courses=["Artificial Intelligence", "Robotics"])
-P2 = Person(age=18, field="Computer Engineering", GPA=4, courses=["Julia"])
+P1 = Person(age=24, field="Mechanical Engineering", courses=["Artificial Intelligence", "Robotics"], ID = 1)
+P2 = Person(age=18, field="Computer Engineering", GPA=4, courses=["Julia"], ID = 2)
 
 U = University(name="Julia University", people=[P1, P2])
 
 print(P1.aml)
 #=
-<person>
+<person id="1">
   <age>24</age>
   <study-field>Mechanical Engineering</study-field>
   <GPA>4.5</GPA>
   <taken-courses>Artificial Intelligence</taken-courses>
   <taken-courses>Robotics</taken-courses>
 </person>
-
 =#
 
 print(P2.aml)
 #=
-<person>
+<person id="2">
   <age>18</age>
   <study-field>Computer Engineering</study-field>
   <GPA>4</GPA>
@@ -42,16 +42,15 @@ print(P2.aml)
 
 print(U.aml)
 #=
-<university>
-  <university-name>Julia University</university-name>
-  <person>
+<university university-name="Julia University">
+  <person id="1">
     <age>24</age>
     <study-field>Mechanical Engineering</study-field>
     <GPA>4.5</GPA>
     <taken-courses>Artificial Intelligence</taken-courses>
     <taken-courses>Robotics</taken-courses>
   </person>
-  <person>
+  <person id="2">
     <age>18</age>
     <study-field>Computer Engineering</study-field>
     <GPA>4</GPA>
--- src/AML.jl
+++ src/AML.jl
@@ -17,8 +17,21 @@ export @a_str
 Use @aml macro to define a Julia type, and then the package automatically creates a xml or html associated with the defined type.
 
 * Specify the root html/xml name in a string after the struct name
+```julia
+@aml struct Person "person"
+```
 * Sepecify the html/xml names for childs in strings in front of the struct fields after `,`
+```julia
+age::UInt, "age"
+```
+* If the value is going to be an attribute put `a` before its name
+```julia
+ID::Int64, a"id"
+```
 * You can specify the default value for a argument by using `= defVal` syntax
+```julia
+GPA::Float64 = 4.5, "GPA"
+```
 
 # Examples
 ```julia
@@ -29,23 +42,24 @@ using AML
     field::String, "study-field"
     GPA::Float64 = 4.5, "GPA"
     courses::Vector{String}, "taken-courses"
+    ID::Int64, a"id"
 end
 
 
 @aml struct University "university"
-    name, "university-name"
+    name, a"university-name"
     people::Vector{Person}, "students"
 end
 
-P1 = Person(age=24, field="Mechanical Engineering", courses=["Artificial Intelligence", "Robotics"])
-P2 = Person(age=18, field="Computer Engineering", GPA=4, courses=["Julia"])
+P1 = Person(age=24, field="Mechanical Engineering", courses=["Artificial Intelligence", "Robotics"], ID = 1)
+P2 = Person(age=18, field="Computer Engineering", GPA=4, courses=["Julia"], ID = 2)
 
 U = University(name="Julia University", people=[P1, P2])
 ```
 
 ```html
 julia> print(P1.aml)
-<person>
+<person id="1">
   <age>24</age>
   <study-field>Mechanical Engineering</study-field>
   <GPA>4.5</GPA>
@@ -53,9 +67,8 @@ julia> print(P1.aml)
   <taken-courses>Robotics</taken-courses>
 </person>
 
-
 julia> print(P2.aml)
-<person>
+<person id="2">
   <age>18</age>
   <study-field>Computer Engineering</study-field>
   <GPA>4</GPA>
@@ -63,16 +76,15 @@ julia> print(P2.aml)
 </person>
 
 julia> print(U.aml)
-<university>
-  <university-name>Julia University</university-name>
-  <person>
+<university university-name="Julia University">
+  <person id="1">
     <age>24</age>
     <study-field>Mechanical Engineering</study-field>
     <GPA>4.5</GPA>
     <taken-courses>Artificial Intelligence</taken-courses>
     <taken-courses>Robotics</taken-courses>
   </person>
-  <person>
+  <person id="2">
     <age>18</age>
     <study-field>Computer Engineering</study-field>
     <GPA>4</GPA>
