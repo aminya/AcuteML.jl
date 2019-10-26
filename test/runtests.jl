ed3e18f3a0b594c061be0139f4bc295dd1fdc422
aminya <aminyahyaabadi74@gmail.com>
2019-10-26 17:31:12 -0500

    tests for extractor

--- test/runtests.jl
+++ test/runtests.jl
@@ -87,3 +87,58 @@ end
 
 
 end
+
+@testset "extractor" begin
+
+    xml = parsexml("""
+    <?xml version="1.0" encoding="UTF-8" standalone="yes"?><!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
+    <university university-name="Julia University">
+      <person id="1">
+        <age>24</age>
+        <study-field>Mechanical Engineering</study-field>
+        <GPA>4.5</GPA>
+        <taken-courses>Artificial Intelligence</taken-courses>
+        <taken-courses>Robotics</taken-courses>
+      </person>
+      <person id="2">
+        <age>18</age>
+        <study-field>Computer Engineering</study-field>
+        <GPA>4</GPA>
+        <taken-courses>Julia</taken-courses>
+      </person>
+    </university>
+    """)
+
+    # extract Doc
+
+    D = Doc(xml)
+
+    # extract University
+
+    U = University(D.university)
+
+    @test U.name == "Julia University"
+
+
+    # extract Person
+
+    P1 = Person(U.people[1])
+
+    @test P1.age == 24
+    @test P1.field == "Mechanical Engineering"
+    @test P1.GPA == 4.5
+    @test P1.courses == ["Artificial Intelligence", "Robotics"]
+    @test P1.ID == 1
+
+    P2 = Person(U.people[2])
+
+    @test print(P2.aml) == print(strip("""
+    <person id="2">
+        <age>18</age>
+        <study-field>Computer Engineering</study-field>
+        <GPA>4</GPA>
+        <taken-courses>Julia</taken-courses>
+      </person>
+    """))
+
+end
