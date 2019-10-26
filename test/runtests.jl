d50b8fdedeef0252bab63fe4998cf44b427f1f6a
aminya <aminyahyaabadi74@gmail.com>
2019-10-26 17:30:59 -0500

    tests for constructor

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,24 +7,30 @@ using Test
     field::String, "study-field"
     GPA::Float64 = 4.5, "GPA"
     courses::Vector{String}, "taken-courses"
+    ID::Int64, a"id"
 end
-
 
 @aml struct University "university"
-    name, "university-name"
-    people::Vector{Person}, "students"
+    name, a"university-name"
+    people::Vector{Person}, "person"
 end
 
-P1 = Person(age=24, field="Mechanical Engineering", courses=["Artificial Intelligence", "Robotics"])
-P2 = Person(age=18, field="Computer Engineering", GPA=4, courses=["Julia"])
+@aml struct Doc xd""
+    university::University, "university"
+end
 
-U = University(name="Julia University", people=[P1, P2])
+@testset "constructor" begin
 
-@testset "AML.jl" begin
+    P1 = Person(age=24, field="Mechanical Engineering", courses=["Artificial Intelligence", "Robotics"], ID = 1)
+    P2 = Person(age=18, field="Computer Engineering", GPA=4, courses=["Julia"], ID = 2)
+
+    U = University(name="Julia University", people=[P1, P2])
+
+    D = Doc(university = U)
 
 
     @test print(P1.aml) == print(strip("""
-    <person>
+    <person id="1">
       <age>24</age>
       <study-field>Mechanical Engineering</study-field>
       <GPA>4.5</GPA>
@@ -34,7 +40,7 @@ U = University(name="Julia University", people=[P1, P2])
     """))
 
     @test print(P2.aml) == print(strip("""
-    <person>
+    <person id="2">
       <age>18</age>
       <study-field>Computer Engineering</study-field>
       <GPA>4</GPA>
@@ -43,16 +49,34 @@ U = University(name="Julia University", people=[P1, P2])
     """))
 
     @test print(U.aml) == print(strip("""
-    <university>
-      <university-name>Julia University</university-name>
-      <person>
+    <university university-name="Julia University">
+      <person id="1">
         <age>24</age>
         <study-field>Mechanical Engineering</study-field>
         <GPA>4.5</GPA>
         <taken-courses>Artificial Intelligence</taken-courses>
         <taken-courses>Robotics</taken-courses>
       </person>
-      <person>
+      <person id="2">
+        <age>18</age>
+        <study-field>Computer Engineering</study-field>
+        <GPA>4</GPA>
+        <taken-courses>Julia</taken-courses>
+      </person>
+    </university>
+    """))
+
+    @test print(D.aml) == print(strip("""
+    <?xml version="1.0" encoding="UTF-8" standalone="yes"?><!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
+    <university university-name="Julia University">
+      <person id="1">
+        <age>24</age>
+        <study-field>Mechanical Engineering</study-field>
+        <GPA>4.5</GPA>
+        <taken-courses>Artificial Intelligence</taken-courses>
+        <taken-courses>Robotics</taken-courses>
+      </person>
+      <person id="2">
         <age>18</age>
         <study-field>Computer Engineering</study-field>
         <GPA>4</GPA>
