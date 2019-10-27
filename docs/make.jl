e8c01524d495890c5b1c60e46451de5218714dbb
aminya <aminyahyaabadi74@gmail.com>
2019-10-27 03:18:43 -0500

    Update doc/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -7,11 +7,14 @@ makedocs(;
     repo="https://github.com/aminya/AML.jl/blob/{commit}{path}#L{line}",
     sitename="AML.jl",
     format=Documenter.HTML(;
-        canonical="https://aminya.github.io/AML.jl",
-        assets=String[],
+        prettyurls = prettyurls = get(ENV, "CI", nothing) == "true",
+        # canonical="https://aminya.github.io/AML.jl",
+        # assets=String[],
     ),
     pages=[
         "Home" => "index.md",
+        "Templating" => "templating.md",
+        "Syntax Reference" => "SyntaxReference.md"
     ],
 )
 
