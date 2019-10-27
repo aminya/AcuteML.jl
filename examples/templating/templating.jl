9ff8a490b4d2846c26b5003217518a939695bc48
aminya <aminyahyaabadi74@gmail.com>
2019-10-27 02:34:23 -0500

    Rendering using Files - example:

---
+++ examples/templating/templating.jl
@@ -0,0 +1,34 @@
+using AML
+
+#################################################################
+# Rendering using Files - example:
+
+# only to set path to current file
+cd(@__DIR__)
+
+
+
+# you can create a file and edit the file directly by using
+newTemplate("person")
+
+# Add the following html code to the generated html file
+#=
+<person id=$(id)>
+  <age>$(age)</age>
+  <study-field>$(field)</study-field>
+  <GPA>$(GPA)</GPA>
+  <taken-courses>$(courses[1])</taken-courses>
+  <taken-courses>$(courses[2])</taken-courses>
+</person>
+=#
+
+# Specify the template (or its path), and also the variables for rendering
+out =render2file("person", false,
+  id = 1,
+  age = 24,
+  field = "Mechanical Engineering",
+  GPA = 4.5,
+  courses = ["Artificial Intelligence", "Robotics"])
+
+# you pass `true` as the 2nd argument to owerwrite person.html statically.
+
