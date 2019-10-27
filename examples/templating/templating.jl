0e79e019d3c23b1379447042c4fa2ccb1899670c
aminya <aminyahyaabadi74@gmail.com>
2019-10-27 03:19:35 -0500

    updating examples for templating

--- examples/templating/templating.jl
+++ examples/templating/templating.jl
@@ -1,7 +1,52 @@
 using AML
 
+# Example 3 - Template Rendering using Functions
+
+# This method only uses functions that return string. You can build your desired string and call the function for rendering.
+
+## create person function to store out html template
+newTemplate("person", :function)
+
+
+function person(;id, age, field, GPA, courses)
+
+  # Build the taken courses section
+  loopOut=""
+  for course in courses
+    loopOut = loopout * """ <taken-courses>$(course)</taken-courses>   """
+  end
+
+  # Append all the sections and varuables together
+  out = """
+  <person id=$(id)>
+    <age>$(age)</age>
+    <study-field>$(field)</study-field>
+    <GPA>$(GPA)</GPA>
+    $loopout
+  </person>
+  """
+
+  return out
+end
+
+# Call the function for rendering
+out = person(
+  id = "1",
+  age = "24",
+  field = "Mechanical Engineering",
+  GPA = "4.5",
+  courses = ["Artificial Intelligence", "Robotics"]
+)
+
+print(out)
+
+# you can also write the output to a file:
+file = open(filePath, "r"); print(file, out); close(file)
+
 #################################################################
-# Rendering using Files - example:
+# Example 4 - Template Rendering using Files
+
+# You can render variables into html/xml files. However, you can't have multiline control flow Julia code in this method.
 
 # only to set path to current file
 cd(@__DIR__)
@@ -31,34 +76,3 @@ out =render2file("person", false,
   courses = ["Artificial Intelligence", "Robotics"])
 
 # you pass `true` as the 2nd argument to owerwrite person.html statically.
-
-#################################################################
-# Rendering using Functions - example:
-
-## create person function to store out html template
-newTemplate("person", :function)
-
-
-function person(;id, age, field, GPA, courses)
-
-"""
-<person id=$(id)>
-  <age>$(age)</age>
-  <study-field>$(field)</study-field>
-  <GPA>$(GPA)</GPA>
-  <taken-courses>$(courses[1])</taken-courses>
-  <taken-courses>$(courses[2])</taken-courses>
-</person>
-"""
-
-end
-
-out = person(
-  id = "1",
-  age = "24",
-  field = "Mechanical Engineering",
-  GPA = "4.5",
-  courses = ["Artificial Intelligence", "Robotics"]
-)
-
-print(out)
