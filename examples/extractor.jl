d7bcd8cb5993f6c0281576190d22b5d144105b5c
aminya <aminyahyaabadi74@gmail.com>
2019-11-02 01:57:46 -0500

    Extractor example updated

--- examples/extractor.jl
+++ examples/extractor.jl
@@ -1,5 +1,41 @@
 using AcuteML
 
+# Type Definition
+@aml struct Person "person", courseCheck
+    age::UInt, "~"
+    field, "study-field"
+    GPA::Float64 = 4.5, "~", GPAcheck
+    courses::Vector{String}, "taken-courses"
+    id::Int64, a"~"
+end
+
+@aml struct University "university"
+    name, a"university-name"
+    people::Vector{Person}, "person"
+end
+
+@aml struct Doc xd""
+    university::University, "~"
+end
+
+# Value Checking Functions
+GPAcheck(x) = x <= 4.5 && x >= 0
+
+function courseCheck(age, field, GPA, courses, id)
+
+    if field == "Mechanical Engineering"
+        relevant = ["Artificial Intelligence", "Robotics", "Machine Design"]
+    elseif field == "Computer Engineering"
+        relevant = ["Julia", "Algorithms"]
+    else
+        error("study field is not known")
+    end
+
+    return any(in.(courses, Ref(relevant)))
+end
+
+
+# Extractor
 xml = parsexml("""
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?><!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
 <university university-name="Julia University">
@@ -18,25 +54,6 @@ xml = parsexml("""
   </person>
 </university>
 """)
-
-GPAcheck(x) = x <= 4.5 && x >= 0
-
-@aml struct Person "person"
-    age::UInt, "~"
-    field, "study-field"
-    GPA::Float64 = 4.5, "~", GPAcheck
-    courses::Vector{String}, "taken-courses"
-    id::Int64, a"~"
-end
-
-@aml struct University "university"
-    name, a"university-name"
-    people::Vector{Person}, "person"
-end
-
-@aml struct Doc xd""
-    university::University, "~"
-end
 
 # extract Doc
 D = Doc(xml) # StructName(xml) like Doc(xml) extracts the data and stores them in proper format
