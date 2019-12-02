4f67ad8285badaf39365e9d615b384da8f850921
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2019-12-02 11:10:02 +0000

    Update precompile_*.jl file

--- deps/SnoopCompile/precompile/precompile_Random.jl
+++ deps/SnoopCompile/precompile/precompile_Random.jl
@@ -1,5 +1,5 @@
 function _precompile_()
     ccall(:jl_generating_output, Cint, ()) == 1 || return nothing
-    precompile(Tuple{typeof(Random.seed!), Array{UInt32, 1}})
     precompile(Tuple{typeof(Random.default_rng)})
+    precompile(Tuple{typeof(Random.seed!), Array{UInt32, 1}})
 end
