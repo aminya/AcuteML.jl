28572fa5ced914bd9d8bf9188393d84f22e04412
aminya <aminyahyaabadi74@gmail.com>
2019-12-02 11:39:49 +0000

    Update precompile_*.jl file

--- deps/SnoopCompile/precompile/precompile_Core.jl
+++ deps/SnoopCompile/precompile/precompile_Core.jl
@@ -1,11 +1,11 @@
 function _precompile_()
     ccall(:jl_generating_output, Cint, ()) == 1 || return nothing
     isdefined(Core, Symbol("#kw#Type")) && precompile(Tuple{getfield(Core, Symbol("#kw#Type")), NamedTuple{(:university,), Tuple{Main.University}}, Type{Main.Doc}})
-    isdefined(Core, Symbol("#kw#Type")) && precompile(Tuple{getfield(Core, Symbol("#kw#Type")), NamedTuple{(:age, :field, :GPA, :courses, :id), Tuple{Int64, String, Int64, Array{String, 1}, Int64}}, Type{Main.Person}})
+    isdefined(Core, Symbol("#kw#Type")) && precompile(Tuple{getfield(Core, Symbol("#kw#Type")), NamedTuple{(:name, :people), Tuple{String, Array{Main.Person, 1}}}, Type{Main.University}})
     isdefined(Core, Symbol("#kw#Type")) && precompile(Tuple{getfield(Core, Symbol("#kw#Type")), NamedTuple{(:university,), Tuple{Main.University}}, Type{Main.Doc}})
     isdefined(Core, Symbol("#kw#Type")) && precompile(Tuple{getfield(Core, Symbol("#kw#Type")), NamedTuple{(:age, :field, :courses, :id), Tuple{Int64, String, Array{String, 1}, Int64}}, Type{Main.Person}})
-    isdefined(Core, Symbol("#kw#Type")) && precompile(Tuple{getfield(Core, Symbol("#kw#Type")), NamedTuple{(:name, :people), Tuple{String, Array{Main.Person, 1}}}, Type{Main.University}})
+    isdefined(Core, Symbol("#kw#Type")) && precompile(Tuple{getfield(Core, Symbol("#kw#Type")), NamedTuple{(:age, :field, :courses, :id), Tuple{Int64, String, Array{String, 1}, Int64}}, Type{Main.Person}})
     isdefined(Core, Symbol("#kw#Type")) && precompile(Tuple{getfield(Core, Symbol("#kw#Type")), NamedTuple{(:name, :people), Tuple{String, Array{Main.Person, 1}}}, Type{Main.University}})
     isdefined(Core, Symbol("#kw#Type")) && precompile(Tuple{getfield(Core, Symbol("#kw#Type")), NamedTuple{(:age, :field, :GPA, :courses, :id), Tuple{Int64, String, Int64, Array{String, 1}, Int64}}, Type{Main.Person}})
-    isdefined(Core, Symbol("#kw#Type")) && precompile(Tuple{getfield(Core, Symbol("#kw#Type")), NamedTuple{(:age, :field, :courses, :id), Tuple{Int64, String, Array{String, 1}, Int64}}, Type{Main.Person}})
+    isdefined(Core, Symbol("#kw#Type")) && precompile(Tuple{getfield(Core, Symbol("#kw#Type")), NamedTuple{(:age, :field, :GPA, :courses, :id), Tuple{Int64, String, Int64, Array{String, 1}, Int64}}, Type{Main.Person}})
 end
