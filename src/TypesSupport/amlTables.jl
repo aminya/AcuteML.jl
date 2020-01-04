fc0fa751426ea7e81e528428763e0e501aa892a6
aminya <aminyahyaabadi74@gmail.com>
2020-01-05 02:52:05 +0330

    amlTables

--- src/TypesSupport/amlTables.jl
+++ src/TypesSupport/amlTables.jl
@@ -3,9 +3,10 @@ import EzXML.parsehtml
 
 function amlTable(x)
   io = IOBuffer()
-  pretty_table(io, x, backend = :html)
+  pretty_table(io, x, backend = :html, standalone = false)
   str = String(resize!(io.data, io.size))
 
-  html = root(parsehtml(str))
+  html = findfirst("html/body/table",parsehtml(str))
+  unlink!(html)
   return html
 end
