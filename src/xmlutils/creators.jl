cf2b2b75523f2baf681f642bd823adf639091f6b
aminya <aminyahyaabadi74@gmail.com>
2020-01-04 14:57:19 +0330

    move docOrElmInit to creators

--- src/xmlutils/creators.jl
+++ src/xmlutils/creators.jl
@@ -1,5 +1,43 @@
 export addelementOne!, addelementVect!
 ################################################################
+# Init
+################################################################
+# doc or element initialize
+"""
+    docOrElmInit(name)
+    docOrElmInit(type, name)
+
+Function to initialize the aml
+
+type:
+0 : element node # default
+10: empty element node
+-1: xml
+-2: html
+"""
+function docOrElmInit(type::Int64 = 0, name::String = nothing)
+
+    if type == 0 # element node
+
+        out = ElementNode(name)
+
+    elseif type == 10 # empty element node
+
+        out = ElementNode(name)
+
+    elseif type == -1 # xml
+
+        out = XMLDocument() # version 1
+
+    elseif type == -2 # html
+
+        out = HTMLDocument() # no URI and external id
+    end
+
+    return out
+end
+
+################################################################
 # Creators
 ################################################################
 
