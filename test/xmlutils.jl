3848092d769134b0ca2d2621002d8682f480bb25
aminya <aminyahyaabadi74@gmail.com>
2020-01-08 19:18:47 +0330

    dhtml xmlutils test

--- test/xmlutils.jl
+++ test/xmlutils.jl
@@ -49,3 +49,48 @@ using AcuteML, Test
     @test string.(["aa", Time(12,53,40), 2]) == findallcontent(typeof(["aa", Time(12,53,40), 2, nothing]), "AnyVect", n, 0)
 
     ################################################################
+    ################################################################
+
+    addelementOne!(dhtml, "normalString", "val", 0)
+    @test "val" == findfirstcontent(String, "normalString", dhtml, 0)
+    @test "val" == findfirstcontent("normalString", dhtml, 0)
+    addelementOne!(dhtml, "attString", "val", 2)
+    @test "val" == findfirstcontent(String, "attString", dhtml, 2)
+
+    using Dates
+    addelementOne!(dhtml, "Date", Date(2013,7,1), 0)
+    @test Date(2013,7,1) == findfirstcontent(Date, "Date", dhtml, 0)
+    addelementOne!(dhtml, "Time", Time(12,53,40), 0)
+    @test Time(12,53,40) == findfirstcontent(Time, "Time", dhtml, 0)
+
+    addelementOne!(dhtml, "DateTime", DateTime(2013,5,1,12,53,40), 0)
+    @test DateTime(2013,5,1,12,53,40) == findfirstcontent(DateTime, "DateTime", dhtml, 0)
+
+    using DataFrames
+    addelementOne!(dhtml, "DataFrame",  DataFrame(course = ["Artificial Intelligence", "Robotics"], professor = ["Prof. A", "Prof. B"] ), 0)
+    @test_skip  DataFrame(course = ["Artificial Intelligence", "Robotics"], professor = ["Prof. A", "Prof. B"] ) == findfirstcontent(DataFrame, "DataFrame", dhtml, 0)
+
+    addelementOne!(dhtml, "Nothing", nothing, 2)
+    @test nothing == findfirstcontent(Nothing, "Nothing", dhtml, 0)
+    @test nothing == findfirstcontent(Union{Nothing,String}, "Nothing", dhtml, 0)
+    ################################################################
+    addelementVect!(dhtml, "stringVect", ["aa", "bb"], 0)
+    @test ["aa", "bb"] == findallcontent(Vector{String}, "stringVect", dhtml, 0)
+
+    addelementVect!(dhtml, "floatVect", [5.6, 7.8], 0)
+    @test [5.6, 7.8] == findallcontent(Vector{Float64}, "floatVect", dhtml, 0)
+
+    addelementVect!(dhtml, "intVect", [5, 6], 0)
+    @test [5, 6] == findallcontent(Vector{Int64}, "intVect", dhtml, 0)
+
+    addelementVect!(dhtml, "DateVect", [Date(2013,7,1), Date(2014,7,1)], 0)
+    @test [Date(2013,7,1), Date(2014,7,1)] == findallcontent(Vector{Date}, "DateVect", dhtml, 0)
+
+    addelementVect!(dhtml, "TimeVect", [Time(12,53,42), Time(12,53,40)], 0)
+    @test [Time(12,53,42), Time(12,53,40)] == findallcontent(Vector{Time}, "TimeVect", dhtml, 0)
+
+    addelementVect!(dhtml, "AnyVect", ["aa", Time(12,53,40), 2, nothing], 0)
+    @test  string.(["aa", Time(12,53,40), 2]) == findallcontent(typeof(["aa", Time(12,53,40), 2, nothing]), "AnyVect", dhtml, 0)
+
+    ################################################################
+    ################################################################
