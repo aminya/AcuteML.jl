3c58109849069e67eccc0c5461a3c59bb28b827c
aminya <aminyahyaabadi74@gmail.com>
2020-01-09 04:17:24 +0330

    [skip ci] Adding benchmark result

--- benchmark/bench.jl
+++ benchmark/bench.jl
@@ -15,7 +15,7 @@ end
     people::Vector{Person}, "person"
 end
 
-@aml mutable struct Doc xd""
+@aml mutable struct Doc "xml"
     university::University, "~"
 end
 
@@ -57,8 +57,51 @@ function mutability(P1, P2, U, D)
     return P1, P2, U, D
 end
 
+function extraction()
+    xml=parsexml("""
+    <?xml version="1.0" encoding="UTF-8"?>
+    <university university-name="MIT">
+      <person id="1">
+        <age>22</age>
+        <study-field>Mechanical Engineering</study-field>
+        <GPA>4.5</GPA>
+        <taken-courses>Artificial Intelligence</taken-courses>    <taken-courses>Robotics</taken-courses>
+      </person>
+      <person id="2">
+        <age>18</age>
+        <study-field>Computer Engineering</study-field>
+        <GPA>4.2</GPA>
+        <taken-courses>Julia</taken-courses>
+      </person>
+    </university>
+    """)
+    D = Doc(xml)
+end
+
 @btime creation();
 
 P1, P2, U, D = creation();
 
 @btime mutability($P1, $P2, $U, $D);
+
+@btime extraction();
+
+#=
+Benchmark Result
+
+------------------------------
+New type system
+
+9.601 μs (92 allocations: 11.34 KiB)
+20.401 μs (72 allocations: 11.58 KiB)
+491.200 μs (348 allocations: 22.61 KiB)
+
+-------------------------------------
+Old Type System
+
+9.999 μs (92 allocations: 11.34 KiB)
+20.900 μs (72 allocations: 11.58 KiB)
+493.499 μs (363 allocations: 23.23 KiB)
+
+
+=#
