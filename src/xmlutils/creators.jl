c633d366ded4c8522f97457bc02d4d3cd24189fe
aminya <aminyahyaabadi74@gmail.com>
2020-02-08 17:15:09 -0600

    using @transform to define specific methods

--- src/xmlutils/creators.jl
+++ src/xmlutils/creators.jl
@@ -1,4 +1,4 @@
-export addelm!, addelm!
+export addelm!
 ################################################################
 # Creators
 ################################################################
@@ -51,7 +51,7 @@ end
 # Nodes
 ################################################################
 # String
-function addelm!(aml::Node, name::String, value::String, argAmlType::Type{<:AbsNormal})
+@transform function addelm!(aml::Node, name::String, value::String, argAmlType::Type{allsubtypes(AbsNormal)})
     if !isnothing(value) # do nothing if value is nothing
         addelement!(aml, name, value)
     end
@@ -64,7 +64,7 @@ function addelm!(aml::Node, name::String, value::String, argAmlType::Type{AbsAtt
 end
 
 # Number, Bool
-function addelm!(aml::Node, name::String, value::T, argAmlType::Type{<:AbsNormal}) where {T<:Union{Number, Bool}}
+@transform function addelm!(aml::Node, name::String, value::T, argAmlType::Type{allsubtypes(AbsNormal)}) where {T<:Union{Number, Bool}}
     if !isnothing(value) # do nothing if value is nothing
         addelement!(aml, name, string(value))
     end
@@ -107,24 +107,12 @@ function addelm!(aml::Node, name::String, value::T, argAmlType::Type{AbsAttribut
     end
 end
 
-function addelm!(aml::Node, name::String, value::Nothing, argAmlType::Type{<:AbsDocOrNode})
-    # do nothing
-end
-
-function addelm!(aml::Node, name::String, value::Nothing, argAmlType::Type{<:AbsNormal})
-    # do nothing
-end
-
-function addelm!(aml::Node, name::String, value::Nothing, argAmlType::Type{AbsAttribute})
-    # do nothing
-end
-
-function addelm!(aml::Node, name::String, value, argAmlType::AbsIgnore)
+@transform function addelm!(aml::Node, name::String, value::Nothing, argAmlType::Type{allsubtypes(AbsDocOrNode)})
     # do nothing
 end
 ################################################################
 
 # Vector
-function addelm!(aml::Node, name::String, value::Vector, argAmlType::Type{<:AbsDocOrNode})
+@transform function addelm!(aml::Node, name::String, value::Vector, argAmlType::Type{allsubtypes(AbsDocOrNode)})
     foreach(x-> addelm!(aml, name, x, argAmlType), value)
 end
