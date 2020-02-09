793fc2edec25e9af2d5b16e69af944dde699a6ed
aminya <aminyahyaabadi74@gmail.com>
2020-02-09 07:19:49 -0600

    using findalllocal and findfirstlocal instead of EzXML methods

--- src/xmlutils/extractors.jl
+++ src/xmlutils/extractors.jl
@@ -19,6 +19,8 @@ end
     findfirstlocal(string, node)
 
 findfirst with ignoring namespaces. It considers element.name for returning the elements
+
+Much faster than EzXML.findfirst
 """
 function findfirstlocal(name::String, node::Node)
     out = nothing # return nothing if nothing is found
@@ -32,9 +34,11 @@ function findfirstlocal(name::String, node::Node)
 end
 
 """
-    findfirstlocal(string, node)
+    findalllocal(string, node)
 
 findalllocal with ignoring namespaces. It considers element.name for returning the elements
+
+Much faster than EzXML.findfirst
 """
 function findalllocal(name::String, node::Node)
     out = Node[]
@@ -68,11 +72,11 @@ findcontent(UInt8,"midi-channel",node, AbsNormal)
 """
 @transform function findcontent(::Type{T},  name::String, node::Node, argAmlType::Type{allsubtypes(AbsNormal)}) where{T<:String} # for strings
 
-    if hasdocument(node)
-        elm = findfirst(name,node)
-    else
+    # if hasdocument(node)
+    #     elm = findfirst(name,node)
+    # else
         elm = findfirstlocal(name,node)
-    end
+    # end
 
     if isnothing(elm) # return nothing if nothing is found
         return nothing
@@ -95,11 +99,11 @@ end
 # Number,Bool
 @transform function findcontent(::Type{T}, name::String, node::Node, argAmlType::Type{allsubtypes(AbsNormal)}) where {T<:Union{Number,Bool}}
 
-    if hasdocument(node)
-        elm = findfirst(name,node)
-    else
+    # if hasdocument(node)
+    #     elm = findfirst(name,node)
+    # else
         elm = findfirstlocal(name,node)
-    end
+    # end
 
     if isnothing(elm) # return nothing if nothing is found
         return nothing
@@ -124,11 +128,11 @@ end
 # for defined types
 function findcontent(::Type{T}, name::String,node::Node, argAmlType::Type{<:AbsNormal}) where {T}
 
-    if hasdocument(node)
-        elm = findfirst(name,node)
-    else
+    # if hasdocument(node)
+    #     elm = findfirst(name,node)
+    # else
         elm = findfirstlocal(name,node)
-    end
+    # end
 
     if isnothing(elm) # return nothing if nothing is found
         return nothing
@@ -173,11 +177,11 @@ end
 
 @transform function findcontent(::Type{Vector{T}}, name::String, node::Node, argAmlType::Type{allsubtypes(AbsNormal)}) where{T<:String} # for strings
 
-    if hasdocument(node)
-        elmsNode = findall(name, node) # a vector of Node elements
-    else
+    # if hasdocument(node)
+    #     elmsNode = findall(name, node) # a vector of Node elements
+    # else
         elmsNode = findalllocal(name, node) # a vector of Node elements
-    end
+    # end
 
     if isnothing(elmsNode)  # return nothing if nothing is found
         return nothing
@@ -206,11 +210,11 @@ end
 # Number,Bool
 @transform function findcontent(::Type{Vector{T}},  name::String, node::Node, argAmlType::Type{allsubtypes(AbsNormal)}) where{T<:Union{Number,Bool}}
 
-    if hasdocument(node)
-        elmsNode = findall(name, node) # a vector of Node elements
-    else
+    # if hasdocument(node)
+    #     elmsNode = findall(name, node) # a vector of Node elements
+    # else
         elmsNode = findalllocal(name, node) # a vector of Node elements
-    end
+    # end
 
     if isnothing(elmsNode) # return nothing if nothing is found
         return nothing
@@ -239,11 +243,11 @@ end
 # for defined types
 function findcontent(::Type{Vector{T}},  name::String, node::Node, argAmlType::Type{<:AbsNormal}) where{T}
 
-    if hasdocument(node)
-        elmsNode = findall(name, node) # a vector of Node elements
-    else
+    # if hasdocument(node)
+    #     elmsNode = findall(name, node) # a vector of Node elements
+    # else
         elmsNode = findalllocal(name, node) # a vector of Node elements
-    end
+    # end
 
     if isnothing(elmsNode) # return nothing if nothing is found
         return nothing
