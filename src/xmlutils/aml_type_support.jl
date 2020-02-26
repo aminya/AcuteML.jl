ad32d2ce4cb0e35471664d9fba03a445e63ac866
aminya <aminyahyaabadi74@gmail.com>
2020-02-26 15:28:26 -0600

    clean type support

--- src/xmlutils/aml_type_support.jl
+++ src/xmlutils/aml_type_support.jl
@@ -1,14 +1,4 @@
 export aml
 import EzXML.parsehtml
 
-# function amlTypesSupport(expr::Expr)
-#   expr.head == :tuple || error("Invalid usage of amlTypeSupport")
-#
-#   numEl = length(expr.args)
-#
-#   var = expr.args[1]
-#
-#
-# end
-
 include("TypesSupport/amlTables.jl")
