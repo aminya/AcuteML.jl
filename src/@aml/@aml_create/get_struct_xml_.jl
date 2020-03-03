1e44b573b2f82161942184793c7869570f2ee6e0
aminya <aminyahyaabadi74@gmail.com>
2020-03-03 03:39:19 -0600

    get_struct_xml custom code

--- src/@aml/@aml_create/get_struct_xml_.jl
+++ src/@aml/@aml_create/get_struct_xml_.jl
@@ -18,51 +18,55 @@ function get_struct_xmlchecker(struct_function, args_var)
     return struct_xmlchecker
 end
 ################################################################
-function get_struct_xmlcreator(T, args_param, struct_xmlchecker, node_initializer, args_xmlcreator, args_var)
+function get_struct_xmlcreator(T, args_param, struct_xmlchecker, node_initializer, args_xmlcreator, args_var, custom_creator_end)
     struct_xmlcreator = quote
         function ($(esc(T)))(; $(esc.(args_param)...))
             $struct_xmlchecker
             $node_initializer
             $(args_xmlcreator...)
+            $(esc(custom_creator_end))
             return ($(esc(T)))($(esc.(args_var)...), aml)
         end
     end
     return struct_xmlcreator
 end
-function get_struct_xmlcreator(SQ, P, args_param, struct_xmlchecker, node_initializer, args_xmlcreator, args_var)
+function get_struct_xmlcreator(SQ, P, args_param, struct_xmlchecker, node_initializer, args_xmlcreator, args_var, custom_creator_end)
     struct_xmlcreator_curly = quote
         function ($(esc(SQ)))(; $(esc.(args_param)...)) where {$(esc.(P)...)}
             $struct_xmlchecker
             $node_initializer
             $(args_xmlcreator...)
+            $(esc(custom_creator_end))
             return ($(esc(SQ)))($(esc.(args_var)...), aml)
         end
     end
     return struct_xmlcreator_curly
 end
 ################################################################
-function get_struct_xmlextractor(T, args_xmlextractor, struct_xmlchecker, args_var)
+function get_struct_xmlextractor(T, args_xmlextractor, struct_xmlchecker, args_var, custom_extractor_end)
     struct_xmlextractor = quote
         function ($(esc(T)))(aml::Union{Document, Node})
             $(args_xmlextractor...)
             $struct_xmlchecker
+            $(esc(custom_extractor_end))
             return ($(esc(T)))($(esc.(args_var)...), aml)
         end
     end
     return struct_xmlextractor
 end
-function get_struct_xmlextractor(SQ, P, args_xmlextractor, struct_xmlchecker, args_var)
+function get_struct_xmlextractor(SQ, P, args_xmlextractor, struct_xmlchecker, args_var, custom_extractor_end)
     struct_xmlextractor_curly = quote
         function ($(esc(SQ)))(aml::Union{Document, Node}) where {$(esc.(P)...)}
             $(args_xmlextractor...)
             $struct_xmlchecker
+            $(esc(custom_extractor_end))
             return ($(esc(SQ)))($(esc.(args_var)...), aml)
         end
     end
     return struct_xmlextractor_curly
 end
 ################################################################
-function get_struct_xmlupdater(is_struct_mutable, T, args_xmludpater, struct_function, args_varcall)
+function get_struct_xmlupdater(is_struct_mutable, T, args_xmludpater, struct_function, args_varcall, custom_updater_end)
     if is_struct_mutable
         # struct_xmlchecker
         if !ismissing(struct_function[1])
@@ -81,6 +85,7 @@ function get_struct_xmlupdater(is_struct_mutable, T, args_xmludpater, struct_fun
                  setfield!(str,name,value)
                  $struct_xmlchecker
                  $(args_xmludpater...)
+                 $(esc(custom_updater_end))
              end
          end
     else
